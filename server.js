d40600bd7a39f9eaf4b89f1aa53592e296094ba3
Ambrose Shokunbi <shokunbitobi@gmail.com>
2022-05-21 20:03:39 -0500

    tested a console log for our candidates table

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-//requiring that express will be used. 
+//requiring that express and mysql12 will be used
 const express = require('express');
+const mysql = require('mysql2');
 
 //creating the port and app for express to be used
 const PORT = process.env.PORT || 3001;
@@ -9,7 +10,23 @@ const app = express();
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // your mysql username,
+        user: 'root',
+        // your my sql passowrd
+        password: 'ashokunb',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
+//return all data in our candidates table
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
 
 //default response for any other reques (NOT FOUND), must be the last thing before port
 app.use((req, res) => {
