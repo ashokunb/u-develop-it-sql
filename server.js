f574f153b521c2c4dd4a9307f6227cba0dc4bd77
Ambrose Shokunbi <shokunbitobi@gmail.com>
2022-05-21 21:35:12 -0500

    tested query for single candidate, delete a candidate, and adding a candidate

--- server.js
+++ server.js
@@ -23,10 +23,39 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
-//return all data in our candidates table
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-  });
+// //return all data in our candidates table
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+//   });
+
+// // GET a signle candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+// //DELETE  a candidate; the id =? is a place holder text for whatever id that we use. 
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+//wer are passing sql and parms into our query that runs the function
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+});
+
 
 //default response for any other reques (NOT FOUND), must be the last thing before port
 app.use((req, res) => {
